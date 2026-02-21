0b2d778a43efef245db8592f7453e0e6883aea54
OneMight <dimamirankov63@gmail.com>
2026-02-21 15:19:53 +0300

    feat: vercel analytics

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,11 +1,14 @@
 import { Header, LoginedHeader } from "@/layouts";
 import { useUser } from "@/lib/useUser";
 import { Outlet } from "@tanstack/react-router";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   const { user, isLoading } = useUser();
   return (
     <main className="w-full bg-main-app/80 min-h-screen flex flex-col items-center max-w-480 gap-15 relative">
+      <Analytics />
+      <SpeedInsights />
       {!isLoading && user ? <LoginedHeader /> : <Header />}
       <div className="w-full flex justify-center items-center">
         <Outlet />
